de6a93f73eeace410769a02971422d210863c34e
Victor McLean <vmvictorvm@gmail.com>
2017-01-19 15:27:44 -0500

    Add project page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    })
+})
+
 // bad - send back json with error message
 app.get('/bad', (req, res) => {
     res.send({
